7258c3155ee0418846b5622ed5fb0132582813c5
pooriaredorg <165016405+pooriaredorg@users.noreply.github.com>
2025-03-16 22:32:30 +0330

    Update fetch_configs.py

--- src/fetch_configs.py
+++ src/fetch_configs.py
@@ -303,7 +303,7 @@ def save_configs(configs: List[str], config: ProxyConfig):
     try:
         os.makedirs(os.path.dirname(config.OUTPUT_FILE), exist_ok=True)
         with open(config.OUTPUT_FILE, 'w', encoding='utf-8') as f:
-            header = """//profile-title: base64:8J+RvUFub255bW91cy3wnZWP
+            header = """//profile-title: base64:8J+lt1BPT1JJQV9SRUQo2b7Ys9ixLdii2LHbjNin24zbjCk=
 //profile-update-interval: 1
 //subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531
 //support-url: https://t.me/BXAMbot
